581830f14a33afb97cf2a8370179fd0bd42d3294
Sviatoslav Prudkyi <sviatoslav.prudkyi@eu.nextiva.local>
2020-05-26 05:18:34 -0700

    menu creation, cards switch on mouse over

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,51 @@
 // MENU
 
-const letfMenu = document.querySelector('.left-menu'),
-    hamburger = document.querySelector('.hamburger');
+const leftMenu = document.querySelector('.left-menu'),
+    hamburger = document.querySelector('.hamburger'),
+    tvShows = document.querySelector('.container'),
+    tvCard = document.querySelectorAll('.tv-card__img')
 
 
 //Open, close of the menu 
 
 hamburger.addEventListener('click', () => {
-    letfMenu.classList.toggle('openMenu');
+    leftMenu.classList.toggle('openMenu');
     hamburger.classList.toggle('open');
 });
 
 document.addEventListener('click', (e) => {
     if (!e.target.closest('.left-menu')) {
-        letfMenu.classList.remove('openMenu');
+        leftMenu.classList.remove('openMenu');
         hamburger.classList.remove('open');
     }
 })
 
-letfMenu.addEventListener('click', event => {
+leftMenu.addEventListener('click', event => {
     const target = event.target;
     const dropdown = target.closest('.dropdown');
     if (dropdown) {
-        dropdown.classList.toggle('.active');
+        dropdown.classList.toggle('active');
+        leftMenu.classList.add('openMenu');
+        hamburger.classList.add('open');
     }
+});
+
+// tvShows.forEach(item => item.addEventListener('mouseover', (e) => {
+
+//     if (e.target.classList.contains('tv-card__img')) {
+//         console.log(item)
+
+//     }
+// }))
+
+
+tvCard.forEach(img => {
+    const imgSrc = img.src;
+    const imgAtr = img.getAttribute('data-backdrop');
+    img.addEventListener('mouseover', () => {
+        img.src = imgAtr;
+    });
+    img.addEventListener('mouseleave', () => {
+        img.src = imgSrc;
+    })
 });
